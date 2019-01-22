abff064fd1e114d62554657f2c211e458e2688cb
huangjixin <517714860@qq.com>
2019-01-22 11:28:10 +0800

    添加excel导出

--- MyVote/src/main/java/com/zhiyesoft/vote/modules/topic/web/VoteController.java
+++ MyVote/src/main/java/com/zhiyesoft/vote/modules/topic/web/VoteController.java
@@ -1,10 +1,14 @@
 package com.zhiyesoft.vote.modules.topic.web;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
@@ -21,11 +25,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.pagehelper.PageInfo;
+import com.zhiyesoft.vote.basic.core.utils.FileUtil;
 import com.zhiyesoft.vote.basic.core.vo.Response;
 import com.zhiyesoft.vote.modules.topic.domain.Customer;
 import com.zhiyesoft.vote.modules.topic.domain.Vote;
 import com.zhiyesoft.vote.modules.topic.service.ICustomerService;
 import com.zhiyesoft.vote.modules.topic.service.IVoteService;
+import com.zhiyesoft.vote.modules.topic.vo.Person;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -134,6 +140,46 @@ public class VoteController {
 		PageInfo<Vote> pageInfo = voteService.selectByExample(null, page, size);
 		response.setData(pageInfo);
 		return response;
+	}
+
+	@RequestMapping("export")
+	public void export(HttpServletResponse response) {
+
+		// 模拟从数据库获取需要导出的数据
+		List<Person> personList = new ArrayList<>();
+		Person person1 = new Person("路飞", "1", new Date());
+		Person person2 = new Person("娜美", "2", new Date());
+		Person person3 = new Person("索隆", "1", new Date());
+		Person person4 = new Person("小狸猫", "1", new Date());
+		personList.add(person1);
+		personList.add(person2);
+		personList.add(person3);
+		personList.add(person4);
+
+		// 导出操作
+		try {
+			FileUtil.exportExcel(personList, "花名册", "草帽一伙", Person.class, "海贼王.xls", response);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@RequestMapping("importExcel")
+	public void importExcel() {
+		String filePath = "F:\\海贼王.xls";
+		// 解析excel，
+		List<Person> personList;
+		try {
+			personList = FileUtil.importExcel(filePath, 1, 1, Person.class);
+			// 也可以使用MultipartFile,使用 FileUtil.importExcel(MultipartFile file, Integer
+			// titleRows, Integer headerRows, Class<T> pojoClass)导入
+			System.out.println("导入数据一共【" + personList.size() + "】行");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// TODO 保存数据库
 	}
 
 }
